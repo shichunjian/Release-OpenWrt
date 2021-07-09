ad4b2044b8da0497e02cdec3272fced878148cc2
shichunjian <33786527+shichunjian@users.noreply.github.com>
2021-07-10 04:21:55 +0800

    Update diy-release1.sh
    更改K2P原版为硬改32M版

--- diy-release1.sh
+++ diy-release1.sh
@@ -7,9 +7,15 @@
 #
 
 # 修改openwrt登陆地址,把下面的192.168.2.1修改成你想要的就可以了，其他的不要动
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
 
-#取消掉feeds.conf.default文件里面的helloworld的#注释
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
+# 取消掉feeds.conf.default文件里面的helloworld的#注释
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
 
+
+# 更改K2P原版为硬改32M版
+sed -i 's/15744k/32128k/g' target/linux/ramips/image/mt7621.mk
+sed -i 's/<80000000>/<10000000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/m25p,fast-read;/broken-flash-reset;/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+sed -i 's/<0xa0000 0xf60000>/<0xa0000 0x1f60000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
