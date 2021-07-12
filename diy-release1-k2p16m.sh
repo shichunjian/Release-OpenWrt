b3be58c1ac2040ffa9e1542550e69dc9c914aec4
shichunjian <33786527+shichunjian@users.noreply.github.com>
2021-07-12 09:22:55 +0800

    Create diy-release1-k2p16m.sh

---
+++ diy-release1-k2p16m.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# 说明：
+# 除了第一行的#!/bin/bash不要动，其他的设置，前面带#表示不起作用，不带的表示起作用了（根据你自己需要打开或者关闭）
+#
+
+# 修改openwrt登陆地址,把下面的192.168.2.1修改成你想要的就可以了，其他的不要动
+# sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+
+# 取消掉feeds.conf.default文件里面的helloworld的#注释
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default  #使用源码自带ShadowSocksR Plus+出国软件
+
+
+# K2P原版内存地址修复
+sed -i 's/<80000000>/<10000000>/g' target/linux/ramips/dts/mt7621_phicomm_k2p.dts
